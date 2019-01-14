da9baa5d2592ef491ec5c8ab0ccc4dc2a73cc9ee
sneha525 <46693007+sneha525@users.noreply.github.com>
2019-01-14 16:59:32 -0500

    webtech class in

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+//Name:Sneha Sahani
+//Date: January 14,2019
+//Student: C0740756
+//Course: CSD 2214 Section 6
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+
+//console.log(endDate.value);
+
